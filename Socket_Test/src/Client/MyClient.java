46a066e3a617c9b313290987bcb513cd6261078a
OUBRAYME-MUSTAFA <102171461+OUBRAYME-MUSTAFA@users.noreply.github.com>
2022-03-25 00:21:23 +0100

    Update MyClient.java

--- Socket_Test/src/Client/MyClient.java
+++ Socket_Test/src/Client/MyClient.java
@@ -1,35 +1,28 @@
-package Client;
+package Serveur;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 
-public class MyClient {
-	public static void main(String arg[]) throws Exception 
-	{
-	 Socket socket2 = new Socket("localhost",3456);
-	 System.out.println("I am connecting ....");
-	 InputStream is = socket2.getInputStream();
-	 OutputStream os = socket2.getOutputStream();
-	 /*
-	 int mynb = 5 ;
-	 System.out.println("I sent   "+mynb+" to the server ");
-	 os.write(mynb);
-	 int res;
-	 res =is.read(); 
-	 System.out.println("I got  this "+res);*/
-	 BufferedReader bf = new BufferedReader(new InputStreamReader(socket2.getInputStream()));
-	 String req= bf.readLine();
-	 System.out.println("i recive this requet : " + req);
-	 
-	 System.out.println("==== Now i'm gonna to sent a String ....");
-	 PrintWriter pw = new PrintWriter(os,true);
-	 pw.println("this just a test");
-	 System.out.println("it's done ....");
+public class MyServeur{
 
-	 socket2.close();
+	public static void main(String arg[]) throws Exception 
+	{   
+		ServerSocket  serveur = new ServerSocket(6534);
+		System.out.println("waiting for someone connecting .......");
+		Socket socket1  = serveur.accept();
+		InputStream is  = socket1.getInputStream();
+		OutputStream os = socket1.getOutputStream();
+	
+	
+		BufferedReader br = new BufferedReader( new InputStreamReader(is));
+		System.out.println("waiting for some characters .......");
+		String s =  br.readLine();
+		System.out.println("it's done and i got this "+s);
+		socket1.close();
 	}
+	
 }
