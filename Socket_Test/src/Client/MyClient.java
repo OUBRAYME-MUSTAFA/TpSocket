2f3778b914a818e8ec7c33908761c431107a3ff2
OUBRAYME-MUSTAFA <102171461+OUBRAYME-MUSTAFA@users.noreply.github.com>
2022-03-25 00:21:54 +0100

    Update MyClient.java

--- Socket_Test/src/Client/MyClient.java
+++ Socket_Test/src/Client/MyClient.java
@@ -1,28 +1,26 @@
-package Serveur;
+package Client;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.ServerSocket;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class MyServeur{
-
+public class MyClient {
 	public static void main(String arg[]) throws Exception 
-	{   
-		ServerSocket  serveur = new ServerSocket(6534);
-		System.out.println("waiting for someone connecting .......");
-		Socket socket1  = serveur.accept();
-		InputStream is  = socket1.getInputStream();
-		OutputStream os = socket1.getOutputStream();
-	
-	
-		BufferedReader br = new BufferedReader( new InputStreamReader(is));
-		System.out.println("waiting for some characters .......");
-		String s =  br.readLine();
-		System.out.println("it's done and i got this "+s);
-		socket1.close();
+	{
+		
+	 Socket socket2 = new Socket("localhost",6534);
+	 System.out.println("I am connecting ....");
+	 PrintWriter    pw = new PrintWriter   (socket2.getOutputStream(),true);
+	 
+	 Scanner scanner = new Scanner(System.in);
+		System.out.print("write something : ");
+		String nb = scanner.next();
+	 System.out.println("I sent   '"+nb+"' to the server ");
+	 pw.println(nb);
+
+	 
+
+
+	 socket2.close();
 	}
-	
 }
